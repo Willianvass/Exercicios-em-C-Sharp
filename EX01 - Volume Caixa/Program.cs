179366a0a370cef4f102adb0fef53f578dbc0570
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-01 23:24:23 -0300

    Add files via upload

---
+++ EX01 - Volume Caixa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EX01___Volume_Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume, comprimento, largura, altura;
+
+            System.Console.Write("Qual altura da caixa: ");
+            altura = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual o comprimento: ");
+            comprimento = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual largura: ");
+            largura = double.Parse(Console.ReadLine());
+
+            volume = comprimento * altura * largura;
+
+            System.Console.WriteLine("Uma caixa de largura " + largura + " altura " + altura + " e comprimento " + comprimento + " tem um volume de " + volume);
+        }
+    }
+}
