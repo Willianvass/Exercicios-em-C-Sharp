914059daf26d814aaebac8eb3f98b20090fd7d1a
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-02 23:54:45 -0300

    Add files via upload

---
+++ EX02 - Converter Temperatura/Program.cs
@@ -0,0 +1,25 @@
+﻿//Converter a temperatura em graus Fahrenheit para graus Celsius. Utiliza console de entrada e saída de dados.
+
+using System;
+
+namespace EX02___Converter_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius;
+            double Fahrenheit;
+
+            System.Console.WriteLine("\n");
+            System.Console.Write("\tQual a temperatura em Fahrenheit a converter: ");
+            Fahrenheit = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("\n");
+
+            Celsius = ((Fahrenheit - 32) * 5 / 9);
+
+            System.Console.WriteLine("\tA temperatura em Celsius é: " + Celsius.ToString("F"));
+            System.Console.WriteLine("\n");
+        }
+    }
+}
