9baee960fa040ed546a6c8823fe763f7af25cc5d
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-04 13:05:08 -0300

    Add files via upload

---
+++ EX03 - Volume Cilindro/Program.cs
@@ -0,0 +1,25 @@
+﻿//Calcular o volume de um Cilindro. Mostra de forma simples a declaração de variáveis e entrada e saída de dados
+//FORMULA: π r² h
+
+using System;
+
+public class Ex03_Volume_Cilindro
+{
+	public static void Main()
+	{
+	    double pi = 3.14;
+	    double altura;
+	    double raio;
+	    double volume;
+	    
+	    Console.Write("Qual a altura do cilindro: ");
+	    altura = double.Parse(Console.ReadLine());
+	    
+	    Console.Write("Qual o raio: ");
+	    raio = double.Parse(Console.ReadLine());
+	    
+	    volume = pi * altura * (raio * raio);
+	    
+	    Console.WriteLine("O volume do cilindro é: " + volume.ToString("F"));
+	}
+}
