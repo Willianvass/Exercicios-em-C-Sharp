9741db275f91154cfb5fc4eb3bd7f87b955be2fc
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-04 13:02:33 -0300

    Add files via upload

---
+++ EX04 - Custo Coelhos/Program.cs
@@ -0,0 +1,27 @@
+﻿//Calcular o custo com a criação de coelhos. 
+//Mostra de forma simples como manipular dados via console e declarar variáveis, e expressões algébricas:
+//FORMULA: (qtdCoelhos * 0.70) / 28
+
+using System;
+
+namespace EX04_CUSTO_COELHOS
+{
+    class Coelhos
+    {
+        static void Main(string[] args)
+        {
+            double custoCoelhos;
+            int qtdCoelhos;
+            Console.Write("Quantos coelhos está criando: ");
+            qtdCoelhos = Int32.Parse(Console.ReadLine());
+
+            custoCoelhos = (qtdCoelhos * 0.70) / 28;
+
+            System.Console.WriteLine("\n");
+            System.Console.WriteLine("O custo para a criação de " + qtdCoelhos + " colehos é de R$ " + custoCoelhos + " por dia!!");
+            System.Console.WriteLine("\n");
+        }
+    }
+}
+
+
