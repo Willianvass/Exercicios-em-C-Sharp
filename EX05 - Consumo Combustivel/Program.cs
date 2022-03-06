23cd1767182e5dd5dd029ff5f4585c88b5ad1c59
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-06 08:12:04 -0300

    Add files via upload

---
+++ EX05 - Consumo Combustivel/Program.cs
@@ -0,0 +1,28 @@
+﻿//Calcular o consumo de combustível por Km. Mostra de forma simples conceitos de entrada e saída de dados e declaração de variáveis:
+
+
+
+using System;
+
+namespace EX05___Consumo_Combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double consumo;
+            double kmPercorrido;
+            double mediaConsumo;
+
+            System.Console.Write("Qual o km percorrido: ");
+            kmPercorrido = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Quanto voce gastou de combustivel: ");
+            consumo = double.Parse(Console.ReadLine());
+
+            mediaConsumo = consumo / kmPercorrido;
+
+            Console.WriteLine("Você percorreu {0} Kms e gastou {1} litros, fez uma média de {2} por Km percorrido!", kmPercorrido, consumo, mediaConsumo.ToString("F"));
+        }
+    }
+}
