7866e463f860402c1bb34e334c39c74444790185
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-15 13:02:43 -0300

    Add files via upload

---
+++ EX11 - Salario Vendedor/Program.cs
@@ -0,0 +1,34 @@
+﻿//Calcular o salário total de um vendedor. 
+//Define pontos importantes de como se utilizar console de entrada e saída de dados
+
+using System;
+
+namespace Salario_Vendedor
+{
+    public class Salario_Vendedor
+    {
+        static void Main(string[] args)
+        {
+            int idFuncionario;
+            double salarioFixo, totalVendas, percentualGanho, salarioTotal, comissão;
+
+            System.Console.Write("Informe o ID do funcionário: ");
+            idFuncionario = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual o valor do salário fixo R$ ");
+            salarioFixo = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Quanto foi o total de vendas no mês R$ ");
+            totalVendas = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual o percentual de comissão nas vendas ");
+            percentualGanho = double.Parse(Console.ReadLine());
+
+            comissão = totalVendas * percentualGanho / 100;
+
+            salarioTotal = comissão + salarioFixo;
+
+            System.Console.WriteLine("Funcionário ID: {0} vendeu R$ {1} deu uma comissão de R$ {2} seu salário total é de R$ {3}", idFuncionario, totalVendas.ToString("C"), comissão.ToString("C"), salarioTotal.ToString("C"));
+        }
+    }
+}
