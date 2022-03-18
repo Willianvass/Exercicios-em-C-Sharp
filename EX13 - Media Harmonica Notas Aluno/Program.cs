dfe69218637b9e6c741523d6e7fb97a105f2659c
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-18 13:25:08 -0300

    Add files via upload

---
+++ EX13 - Media Harmonica Notas Aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿//Calcular a média harmônica das notas de um aluno. Define como utilizar uma console manipulando dados de entrada e saída e declarando variáveis
+
+using System;
+
+namespace Nota_Harmonica_Aluno
+{
+    public  class Nota_Harmonica_Aluno
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4, mediaHarmonica;
+
+            System.Console.Write("Qual a nota da primeira prova: ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual a nota da segunda prova: ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual a nota da terceira prova: ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual a nota da quarta prova: ");
+            nota4 = double.Parse(Console.ReadLine());
+
+            mediaHarmonica = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            //MEDIAH = (4/((1/N1) + (1/N2) + (1/N3) + (1/N4)));
+            System.Console.WriteLine("Após as 4 provas voce teve uma média de: "), mediaHarmonica;
+        }
+    }
+}
