3f9142ac5bbc1e62d5d305cd5689ba4690e703c6
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-20 17:29:50 -0300

    Add files via upload

---
+++ EX17 - Quadrado de Numero/Program.cs
@@ -0,0 +1,22 @@
+﻿//Calcular o quadrado de um número qualquer. Simples utilização de entrada e saída de dados via console e o método Math.Pow
+//O quadrado de um número inteiro é calculado através da potenciação da base inteira em relação ao expoente de número dois. Dessa forma estamos multiplicando o número inteiro por ele mesmo
+
+using System;
+
+namespace Quadrado_Numero
+{
+    public class Quadrado_Numero
+    {
+        public static void Main(string[] args)
+        {
+            double numBase, resultado;
+
+            System.Console.Write("Qual o numero: ");
+            numBase = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(numBase, 2);
+
+            System.Console.WriteLine("O quadrado o numero {0} é de: {1}", numBase, resultado);
+        }
+    }
+}
