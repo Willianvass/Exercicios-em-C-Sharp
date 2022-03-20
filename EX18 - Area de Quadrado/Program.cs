b8201e25bbfec7e83bcf63055aed3fdb37c44f8a
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-20 17:30:18 -0300

    Add files via upload

---
+++ EX18 - Area de Quadrado/Program.cs
@@ -0,0 +1,22 @@
+﻿//Calcular a área de um quadrado. Demonstra a simples utilização de entrada e saída de dados
+//FORMULA: lado X lado
+
+using System;
+
+namespace Area_Quadrado
+{
+    public class Area_Quadrado
+    {
+        public static void Main(string[] args)
+        {
+            double lado, area;
+
+            System.Console.Write("Qual o comprimento do primeiro lado: ");
+            lado = double.Parse(Console.ReadLine());
+
+            area = (lado * lado);
+
+            System.Console.WriteLine("A area de um quadrado de lados de {0} é de: {1}", lado, area.ToString("F"));
+        }
+    }
+}
