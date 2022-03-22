243fc93664c97d2d70ce6fb376264d67ae5610a9
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-21 23:09:42 -0300

    Add files via upload

---
+++ EX19 - Consumo Combustivel em uma Viagem/Program.cs
@@ -0,0 +1,29 @@
+﻿//Calcular o consumo de combustível, ou seja a quantidade de litros consumidos em uma viagem de veículo. Utiliza de forma simples e clara conceitos de declaração de variáveis e console;
+
+using System;
+
+namespace Consumo_Viagem
+{
+    public class Consumo_Viagem
+    {
+        public static void Main(string[] args)
+        {
+            double tempo, velocidade, kmPorLitro, distancia, consumoCombustivel;
+
+            System.Console.Write("Quanto tempo de viagem em horas: ");
+            tempo = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual foi a velocidade média em Km/h: ");
+            velocidade = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Quantos Kms por litro faz o veiculo: ");
+            kmPorLitro = double.Parse(Console.ReadLine());
+            distancia = (velocidade * tempo);
+            consumoCombustivel = (distancia / kmPorLitro);
+            
+            System.Console.WriteLine("A distancia percorrida foi de: {0} Kms", distancia);
+
+            System.Console.WriteLine("A quantidade de combustivel gasto na viagem foi de: {0} litros", consumoCombustivel.ToString("F"));
+        }
+    }
+}
