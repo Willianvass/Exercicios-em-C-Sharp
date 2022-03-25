4320830648dd93732f51f8832ca68123b47b5048
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-24 23:15:24 -0300

    Add files via upload

---
+++ EX21 - Area Triangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿//Apresentar conceitos de entrada e saída de dados. Realizar o cálculo da área de um triângulo.
+//FORMULA: (B x H) / 2
+
+using System;
+
+namespace Area_Triangulo
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            double baseT, altura, areaTriangulo;
+
+            System.Console.Write("Qual a base do triangulo: ");
+            baseT = double.Parse(Console.ReadLine());
+
+            System.Console.Write("Qual a altura do triangulo: ");
+            altura = double.Parse(Console.ReadLine());
+
+            areaTriangulo = (baseT * altura) / 2;
+
+            System.Console.WriteLine("Um triangulo com base {0} e altura de {1} tem uma area de {2}", baseT, altura, areaTriangulo);
+        }
+    }
+}
