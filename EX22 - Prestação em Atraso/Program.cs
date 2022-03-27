3cfec82b58d0b178c515577b1e8b05cc1227da29
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-26 22:40:53 -0300

    Add files via upload

---
+++ EX22 - Prestação em Atraso/Program.cs
@@ -0,0 +1,27 @@
+﻿//Calcular o valor de uma prestação em atraso. Mostra de forma simples como utilizar uma console com entrada e saída de dados e declarar variáveis.
+
+using System;
+
+namespace Prestacao_Atraso
+{
+    public class MyClass
+    {
+        static public void Main(string[] args)
+        {
+            int dias;
+            double prestação, taxaJuros, prestaçãoAtual;
+
+            System.Console.Write("Esta atrado a quantos dias a prestação: ");
+            dias = int.Parse(Console.ReadLine());
+            System.Console.Write("Qual o valor da prestação R$ ");
+            prestação = double.Parse(Console.ReadLine());
+            System.Console.Write("Qual a taxa de juros: ");
+            taxaJuros = double.Parse(Console.ReadLine());
+
+            prestaçãoAtual = ((prestação * taxaJuros / 100) * dias) + prestação;
+
+            System.Console.WriteLine("Devido o atraso de {0} dias no pagamento da prestação, sua nova parcela com juros é de R$ {1}", dias, prestaçãoAtual.ToString("C"));
+
+        }
+    }   
+}
