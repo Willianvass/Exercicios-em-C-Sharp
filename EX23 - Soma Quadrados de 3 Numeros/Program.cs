03917381dc42a5d988e9585c85bc9ef6cfdee415
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-26 22:41:52 -0300

    Add files via upload

---
+++ EX23 - Soma Quadrados de 3 Numeros/Program.cs
@@ -0,0 +1,26 @@
+﻿//Calcular a soma dos quadrados de 3 números. Usa conceitos simples de entrada e saída de dados e declaração de variáveis.
+
+using System;
+
+namespace Soma_Quadrados
+{
+    public class MyClass
+    {
+        static public void Main(string[] args)
+        {
+            double num1, num2, num3, total;
+
+            System.Console.Write("Informe o primeiro numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            System.Console.Write("Informe o segundo numero: ");
+            num2 = double.Parse(Console.ReadLine());
+            System.Console.Write("Informe o terceiro numero: ");
+            num3 = double.Parse(Console.ReadLine());
+
+            //total = (num1 * num1) + (num2 * num2) + (num3 * num3);
+            total = (Math.Pow(num1, 2) + Math.Pow(num2, 2) + Math.Pow(num3, 2));
+
+            System.Console.WriteLine("A soma dos quadrados dos nummeros é: " + total);
+        }
+    }
+}
