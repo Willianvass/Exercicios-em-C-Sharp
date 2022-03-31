5fed4cd254c59e6fc85e865663ba99883b6dc0d8
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-30 23:11:58 -0300

    Add files via upload

---
+++ EX26 - Velocidade Veiculo/Program.cs
@@ -0,0 +1,21 @@
+﻿//Calcular a velocidade média de um veículo qualquer. Mostra de forma simples a utilização de entrada e saída de dados via console e como declarar variáveis.
+
+using System;
+
+namespace Velocidade
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            double distancia, velocidade, tempo;
+            System.Console.Write("Qual a distancia percorrida: ");
+            distancia = double.Parse(Console.ReadLine());
+            System.Console.Write("Quanto tempo demorou: ");
+            tempo = double.Parse(Console.ReadLine());
+
+            velocidade = (distancia / tempo);
+            System.Console.Write($"A velocidade média do veículo é de {velocidade.ToString("N2")} km/h.");
+        }
+    }
+}
