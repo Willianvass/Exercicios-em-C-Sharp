e807e5ccb728f707db82d3ba4d73f7fb09f1fa05
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-03-31 23:25:38 -0300

    Add files via upload

---
+++ EX27 - Verifica Numero Informado/Program.cs
@@ -0,0 +1,29 @@
+﻿//Este programa utiliza conceitos de estruturas de seleção, operadores relacionais e utilização de blocos de instruções. Verificar em que faixa está localizada um número informado pelo usuário.
+//Verificar se um numero esta entre 20 e 90
+
+
+using System;
+
+namespace Verifica_Numero
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            double num;
+            System.Console.Write("Informe um numero: ");
+            num = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x");
+
+            if ((num >= 20) && (num <= 90))
+            {
+                System.Console.WriteLine("O numero esta na faixa de 20 a 90.");
+            }
+            else
+            {
+                System.Console.WriteLine("O numero esta fora da faixa!!");
+            }
+            System.Console.WriteLine("x-x-x-x-x-x-x-x-x-x-x-x-x--x-x-x-x");
+        }
+    }
+}
