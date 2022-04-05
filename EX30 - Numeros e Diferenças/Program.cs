6d050822a17bf3d4584eda11966f1aa9aab877f6
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-04-04 21:54:40 -0300

    Add files via upload

---
+++ EX30 - Numeros e Diferenças/Program.cs
@@ -0,0 +1,30 @@
+﻿//Ler dois valores numéricos e apresentar a diferença do maior pelo menor.
+
+using System;
+
+namespace Numeros_Diferencas
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, diferenca;
+            Console.WriteLine("Qual o primeiro numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o segundo numero: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                diferenca = num1 - num2;
+                Console.WriteLine("A diferença de {0} - {1} é de {2}", num1, num2, diferenca);
+            }
+            else
+            {
+                diferenca = num2 - num1;
+                Console.WriteLine("A difereça de {0} - {1} é de {2}", num2, num1, diferenca);
+            }
+
+        }
+    }
+}
