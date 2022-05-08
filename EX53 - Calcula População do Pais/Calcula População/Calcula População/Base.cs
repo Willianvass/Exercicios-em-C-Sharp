bc5d755f2fc34147727e2160f85d2b35b6f280eb
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-08 07:30:08 -0300

    Add files via upload

---
+++ EX53 - Calcula População do Pais/Calcula População/Calcula População/Base.cs
@@ -0,0 +1,8 @@
+﻿// Estruturas de repetição while. Calcula o tempo necessário para que um País A se iguale ou ultrapasse o número de habitantes de um País B.
+
+namespace Calcula_Popução_Pais
+{
+    internal class Base
+    {
+    }
+}
