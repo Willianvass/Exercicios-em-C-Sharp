3324dcc7ea404cd272caae4a077d549ce1dade7a
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-16 22:28:26 -0300

    Add files via upload

---
+++ EX57 - Lee Numeros Intervalo/Lee NUmeros Intervalo/Lee NUmeros Intervalo/Program.cs
@@ -0,0 +1,29 @@
+﻿//Utilização da estrutura de seleção simples If com estrutura de repetição While. Este programa tem por finalidade ler um número qualquer menor ou igual a 50 e apresentar o valor obtido da multiplicação sucessiva do número por 3 (N * 3) enquanto o produto for menor que 250.
+
+using System;
+
+namespace Lee_NUmeros_Intervalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, produto;
+            produto = 0;
+
+            Console.WriteLine("Informe um numero: ");
+            num = Int32.Parse(Console.ReadLine());
+            
+            if (num <= 50)
+            {
+                produto = num * 3;
+
+                while (produto < 250)
+                {
+                    Console.WriteLine("O produto é {0}", produto);
+                    produto = produto * 3;
+                }
+            }
+        }
+    }
+}
