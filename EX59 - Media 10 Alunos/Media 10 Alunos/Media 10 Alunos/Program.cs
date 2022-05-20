1a37e3dc233e9c06a47383f381abc1d18c197f76
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-19 21:20:34 -0300

    Add files via upload

---
+++ EX59 - Media 10 Alunos/Media 10 Alunos/Media 10 Alunos/Program.cs
@@ -0,0 +1,29 @@
+﻿//Este programa calcula a média de uma turma de 10 alunos. Utiliza métodos de console, estrutura de repetição while e inicialização de variáveis.
+
+using System;
+
+namespace Media_10_Alunos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador;
+            double media, nota, total;
+            contador = 1;
+            total = 0;
+
+            Console.WriteLine("Entre com a {0} a nota: ", contador);
+            nota = double.Parse(Console.ReadLine());
+
+            total = nota + total;
+            contador = contador++;
+
+            media = total / 10;
+
+            Console.WriteLine("A media da classe é: " + media);
+        }
+        
+
+    }
+}
