c41abfd6b49ac682afcf9259bd1b004db0febc40
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-19 21:20:03 -0300

    Add files via upload

---
+++ EX58 - Genero Sexual/Genero Sexual/Genero Sexual/Program.cs
@@ -0,0 +1,25 @@
+﻿//Este programa tem por finalidade verificar o sexo de uma pessoa. Demonstra como utilizar operadores lógicos, relacionais e declarar strings.
+
+using System;
+
+namespace Genero_Sexual
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo;
+            Console.WriteLine("Qual seu sexo: ");
+            sexo = Console.ReadLine();
+
+            if ((sexo == "MASCULINO") || (sexo == "Masculino") || (sexo == "masculino") || (sexo == "FEMININO") || (sexo == "Feminino") || (sexo == "feminino"))
+                {
+                Console.WriteLine("Seu genero é valido!!");
+            }
+            else
+            {
+                Console.WriteLine("Seu sexo é inválido!!");
+            }
+        }
+    }
+}
