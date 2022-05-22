4c7df13a50e2cc25bd42deb86b8ed3af6f4e092c
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-22 17:22:29 -0300

    Add files via upload

---
+++ EX60 - Impares Entre 0 e 10/Impares Entre 0 e 10/Impares Entre 0 e 10/Program.cs
@@ -0,0 +1,25 @@
+﻿//Este programa tem por finalidade apresentar os números ímpares situados na faixa de 0 a 10. Demonstra a utilização da estrutura de repetição while, contadores e incremento.
+
+using System;
+
+namespace Impares_Entre_0_e_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, contador;
+            contador = 1;
+            n = 1;
+
+            Console.WriteLine("Exibe impares entre 0 e 10.");
+
+            while (n < 10)
+            {
+                Console.WriteLine("\n\t\t\t\tO {0}º número ímpar é: {1}", contador, n);
+                n += 2;
+                contador++;
+            }
+        }
+    }
+}
