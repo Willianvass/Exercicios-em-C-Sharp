7b216e3005dd32815d1fa07beb6cf8b4b281adf6
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-22 17:23:00 -0300

    Add files via upload

---
+++ EX61 - Numeros Extrai Cada Digito/Numeros Extrai Digito/Numeros Extrai Digito/Program.cs
@@ -0,0 +1,24 @@
+﻿//Este programa recebe um número com 4 dígitos entre 1000 e 9999 e extrai cada dígito. Utiliza os operadores módulos, métodos de console.
+
+using System;
+
+namespace Numeros_Extrai_Digito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dig1, dig2, dig3, dig4;
+
+            Console.WriteLine("Numero de 0 a 9999: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            dig1 = (num / 1000) % 10; 
+            dig2 = (num / 100) % 10; 
+            dig3 = (num / 10) % 10; 
+            dig4 = (num % 10);
+
+            Console.WriteLine("\n\t\tO número fornecido é: " + dig1 + " " + dig2 + " " + dig3 + " " + dig4 + "\n\n");
+        }
+    }
+}
