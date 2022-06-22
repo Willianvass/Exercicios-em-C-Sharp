95dbb62e28acc085259c37b65ccc2b8000d53801
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-06-21 21:59:52 -0300

    Add files via upload

---
+++ Desafio/Desconto Progressivo/Desconto Progressivo/Desconto Progressivo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Desconto_Progressivo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, desconto, total;
+            Console.WriteLine("Qual valor total da compra R$ ");
+            valor = double.Parse(Console.ReadLine());
+
+            if ((valor >= 400)&&(valor <= 499))
+            {
+                total = valor - (valor * 0.01);
+                Console.WriteLine("Desconto de 1% - Valor total de R$ " + total.ToString("0.00"));
+            }
+        }
+    }
+}
