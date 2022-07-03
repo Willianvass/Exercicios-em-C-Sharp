8a81e447f06e44d6c112caf57ac30c3aaaeb8be2
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-07-03 11:15:00 -0300

    Add files via upload

---
+++ Medidas Triangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Medidas_Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal troco;
+
+            troco = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTAS:");
+            Console.WriteLine("{0} nota(s) de R$ 100.00", (int) troco / 100);
+            troco = troco % 100;
+            Console.WriteLine("{0} nota(s) de R$ 50.00", (int) troco / 50);
+            troco = troco % 50;
+            Console.WriteLine("{0} nota(s) de R$ 20.00", (int) troco / 20);
+            troco = troco % 20;
+            Console.WriteLine("{0} nota(s) de R$ 10.00", (int) troco / 10);
+            troco = troco % 10;
+            Console.WriteLine("{0} nota(s) de R$ 5.00", (int) troco / 5);
+            troco = troco % 5;
+            Console.WriteLine("{0} nota(s) de R$ 2.00", (int) troco / 2);
+            troco = troco % 2;
+            troco = troco * 100;
+
+            Console.WriteLine("MOEDAS:");
+            Console.WriteLine("{0} moeda(s) de R$ 1.00", (int)troco / 100);
+            troco = troco % 100;
+            Console.WriteLine("{0} moeda(s) de R$ 0.50", (int)troco / 50);
+            troco = troco % 50;
+            Console.WriteLine("{0} moeda(s) de R$ 0.25", (int)troco / 25);
+            troco = troco % 25;
+            Console.WriteLine("{0} moeda(s) de R$ 0.10", (int)troco / 10);
+            troco = troco % 10;
+            Console.WriteLine("{0} moeda(s) de R$ 0.05", (int)troco / 5);
+            troco = troco % 5;
+            Console.WriteLine("{0} moeda(s) de R$ 0.01", (int)troco);
+        }
+    }
+}
---
+++ Troco em Dinheiro/Troco/Troco/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Troco
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            double valorVenda, c100, c50, c20, c10, c5, c2, c1, c050, c025, c010, c005 ,c001;
+            Console.Write(" ");
+            valorVenda = double.Parse(Console.ReadLine());
+
+            c100 = valorVenda / 100;
+            c50 = valorVenda / 50;
+            c20 = valorVenda / 20;
+            c10 = valorVenda / 10;
+            c5 = valorVenda / 5;
+            c2 = valorVenda / 2;
+            c1 = valorVenda / 1;
+            c050 = valorVenda / 0.5;
+            c025 = valorVenda / 0.25;
+            c010 = valorVenda / 0.10;
+            c005 = valorVenda / 0.05;
+            c001 = valorVenda / 0.01;
+
+            Console.WriteLine("{0} nota(s) de R$ 100,00", Math.Truncate(c100));
+            Console.WriteLine("{0} nota(s) de R$ 50,00", Math.Truncate(c50));
+            Console.WriteLine("{0} nota(s) de R$ 20,00", Math.Truncate(c20));
+            Console.WriteLine("{0} nota(s) de R$ 10,00", Math.Truncate(c10));
+            Console.WriteLine("{0} nota(s) de R$ 5,00", Math.Truncate(c5));
+            Console.WriteLine("{0} nota(s) de R$ 2,00", Math.Truncate(c2));
+            Console.WriteLine("{0} nota(s) de R$ 1,00", Math.Truncate(c1));
+            Console.WriteLine("MOEDAS:");
+            Console.WriteLine("{0} moeda(s) de R$ 0,50", Math.Truncate(c050));
+            Console.WriteLine("{0} moeda(s) de R$ 0,25", Math.Truncate(c025));
+            Console.WriteLine("{0} moeda(s) de R$ 0,10", Math.Truncate(c010));
+            Console.WriteLine("{0} moeda(s) de R$ 0,05", Math.Truncate(c005));
+            Console.WriteLine("{0} moeda(s) de R$ 0,01", Math.Truncate(c001));
+
+        }
+    }
+}
